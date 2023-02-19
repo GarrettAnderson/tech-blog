f1bac0019cc58c9051bd4403b8a68e4a1a8878a2
Garrett <garrettleegrahamanderson@gmail.com>
2023-02-19 17:33:27 -0500

    working on updating the UI and UX to match the mockup. created Comment table

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 const User = require('./User');
 const Project = require('./Project');
+const Comment = require('./Comment');
+const { post } = require('../controllers/homeRoutes');
 
 User.hasMany(Project, {
   foreignKey: 'user_id',
@@ -9,5 +11,25 @@ User.hasMany(Project, {
 Project.belongsTo(User, {
   foreignKey: 'user_id'
 });
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Project, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
+
+Project.hasMany(Comment, {
+  foreignKey: 'project_id',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+})
 
 module.exports = { User, Project };
